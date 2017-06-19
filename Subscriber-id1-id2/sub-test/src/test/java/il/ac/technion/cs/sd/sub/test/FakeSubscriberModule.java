b71d94a10db849175dad79ccc86e2e6807d913d9
dormaayn <dor.d.ma@gmail.com>
2017-06-19 23:36:43 +0300

    Added Facebook Tests

---
+++ Subscriber-id1-id2/sub-test/src/test/java/il/ac/technion/cs/sd/sub/test/FakeSubscriberModule.java
@@ -0,0 +1,29 @@
+package il.ac.technion.cs.sd.sub.test;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+import il.ac.technion.cs.sd.sub.app.SubscriberInitializer;
+import il.ac.technion.cs.sd.sub.app.SubscriberInitializerImpl;
+import il.ac.technion.cs.sd.sub.app.SubscriberReader;
+import il.ac.technion.cs.sd.sub.app.SubscriberReaderImpl;
+import il.ac.technion.cs.sd.sub.ext.FutureLineStorageFactory;
+
+public class FakeSubscriberModule extends AbstractModule {
+	@Override
+	protected void configure() {
+		bind(SubscriberInitializer.class).to(SubscriberInitializerImpl.class);
+		bind(SubscriberReader.class).to(SubscriberReaderImpl.class);
+		bind(FutureLineStorageFactory.class).toProvider(FakeFactoryProvider.class);
+	}
+}
+
+@Singleton
+class FakeFactoryProvider implements Provider<FutureLineStorageFactoryFake> {
+	static FutureLineStorageFactoryFake ret = new FutureLineStorageFactoryFake();
+
+	@Override
+	public FutureLineStorageFactoryFake get() {
+		return ret;
+	}
+}
